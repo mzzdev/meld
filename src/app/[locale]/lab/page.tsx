6259e9169f154a7a72ee4bc40941e8eca67f70c9
Pablo Belló <pablobello0997@gmail.com>
2024-07-24 14:36:52 +0200

    updated landing and lab page styles

--- src/app/[locale]/lab/page.tsx
+++ src/app/[locale]/lab/page.tsx
@@ -3,17 +3,19 @@ import { HomeIcon } from '@heroicons/react/24/solid';
 import Image from 'next/image';
 import { useTranslations } from 'next-intl';
 import { Suspense } from 'react';
+import { AuroraBackground } from '@/components/ui/aurora-background';
 
 export default function Lab() {
   const t = useTranslations("Lab");
 
   return (
-    <main className="flex flex-col min-h-screen relative overflow-x-hidden cursor-default">
-      <div className="flex flex-col items-center justify-center h-screen w-screen gap-8 dark:bg-black bg-white dark:bg-dot-white/[0.2] bg-dot-black/[0.2]">
-        <h1 className="z-10 my-8 text-9xl text-white dark:text-black bg-black dark:bg-white rounded-lg tracking-tight hover:animate-second transition-transform-background">Lab</h1>
-        <div className="absolute inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_50%,black)]"></div>
+    <main className="flex flex-col min-h-screen relative overflow-x-hidden cursor-default dark:bg-black bg-white dark:bg-dot-white/[0.2] bg-dot-black/[0.2]">
+      <AuroraBackground className="flex flex-col items-center justify-center h-screen w-screen gap-8">
+        <div className="relative flex flex-col items-center justify-center">
+          <p className="text-5xl md:text-7xl font-bold dark:text-white text-center tracking-tight">Lab</p>
+        </div>
         <div className="max-w-[50vw] w-full grid grid-cols-1 md:grid-cols-2 gap-4">
-          <Link href="/lab/glsl" className='col-span-1 h-[40vh] hover:scale-105 hover:shadow-xl transition-all'>
+          <Link href="/lab/glsl" className='col-span-1 h-auto hover:scale-105 hover:shadow-xl transition-all'>
             <Card className="h-full w-full relative">
               <CardHeader className="absolute z-10 top-1 flex-col !items-start mix-blend-difference">
                 <p className="text-tiny text-white uppercase font-bold">GLSL</p>
@@ -31,7 +33,7 @@ export default function Lab() {
               </Suspense>
             </Card>
           </Link>
-          <Link href="/lab/loader" className='col-span-1 h-[40vh] hover:scale-105 hover:shadow-xl transition-all'>
+          <Link href="/lab/loader" className='col-span-1 h-auto hover:scale-105 hover:shadow-xl transition-all'>
             <Card className="h-full w-full relative">
               <CardHeader className="absolute z-10 top-1 flex-col !items-start mix-blend-difference">
                 <p className="text-tiny text-white uppercase font-bold">Loader</p>
@@ -50,8 +52,8 @@ export default function Lab() {
             </Card>
           </Link>
         </div>
-        <Link href="/" title={t('home')} aria-label={t('home')} role="button" className="my-5 w-10 text-black dark:text-white hover:opacity-50"><HomeIcon /></Link>
-      </div>
+        <Link href="/" title={t('home')} aria-label={t('home')} role="button" className="w-10 text-black dark:text-white hover:opacity-50"><HomeIcon /></Link>
+      </AuroraBackground>
     </main>
   );
 }
--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -27,63 +27,60 @@ export default function Home() {
     <main className="flex flex-col min-h-screen relative overflow-x-hidden cursor-default">
 
       {/* logo screen */}
-      <div className="relative w-full h-screen flex flex-col justify-center items-center bg-purple-950 z-10 pointer-events-auto">
+      <div className="relative w-full h-screen flex flex-col justify-center items-center bg-white dark:bg-black z-10 pointer-events-auto">
         <BackgroundGradientAnimation />
-        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-          <Link href="/lab">
-            <div className="bg-white dark:bg-black m-10 p-2 md:p-10 rounded-xl ring-1 ring-inset ring-neutral-200 dark:ring-neutral-800 pointer-events-auto cursor-pointer transition-all dark:hover:ring-purple-700 dark:hover:ring-2 hover:scale-105">
-              <HeroHighlight className="flex flex-col items-center gap-10 md:flex-row md:gap-32">
-                <motion.div initial={{ filter: "blur(5px)", opacity: 0 }} whileInView={{ filter: "blur(0)", opacity: 1 }} viewport={{ once: true }} transition={{ duration: .5, delay: 0, ease: "linear" }}>
-                  <p className="max-w-64 text-5xl leading-tight font-semibold tracking-tight text-black dark:text-white">
-                    <Highlight className=""><span className="">Meld</span></Highlight> {t('logoCard.title')}
-                  </p>
-                </motion.div>
-                <Image
+        <Link href="/lab">
+          <div className="flex flex-col justify-center items-center p-4 md:p-10 lg:flex-row lg:gap-10 rounded-xl bg-white dark:bg-black ring-1 ring-inset ring-neutral-200 dark:ring-neutral-800 pointer-events-auto cursor-pointer transition-all dark:hover:ring-purple-700 dark:hover:ring-2 hover:scale-105 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+            <HeroHighlight className="flex flex-col lg:flex-row items-center gap-5 md:gap-10">
+              <motion.div initial={{ filter: "blur(5px)", opacity: 0 }} whileInView={{ filter: "blur(0)", opacity: 1 }} viewport={{ once: true }} transition={{ duration: .5, delay: 0, ease: "linear" }}>
+                <p className="text-center md:text-left text-2xl md:text-[3rem] md:leading-[1.1] font-semibold tracking-tight text-black dark:text-white">
+                  <Highlight>Meld</Highlight> {t('logoCard.title')}
+                </p>
+              </motion.div>
+              <Image
                 priority
-                  src={'/icons/logo/logo.png'}
-                  alt={'Meld logo'}
-                  width={375}
-                  height={375}
-                  className="pointer-events-none select-none"
-                />
-              </HeroHighlight>
-            </div>
-          </Link>
-        </div>
+                src={'/icons/logo/logo.png'}
+                alt={'Meld logo'}
+                width={375}
+                height={375}
+                className="w-full md:w-1/2 max-w-full max-h-full object-contain pointer-events-none select-none"
+              />
+            </HeroHighlight>
+          </div>
+        </Link>
       </div>
 
       <Divider className="bg-neutral-200 dark:bg-neutral-800" />
 
       {/* nextjs & react */}
-      <div className="relative flex flex-col md:flex-row w-full h-[50vh]">
-        <div className="h-full w-full relative flex flex-col items-center justify-center dark:bg-black bg-white dark:bg-grid-white/[0.2] bg-grid-black/[0.2]">
+      <div className="relative flex flex-col md:flex-row w-full h-[75vh]">
+        <div className="h-full w-full relative flex flex-col items-center justify-center dark:bg-black bg-white dark:bg-grid-white/[0.2] bg-grid-black/[0.2] gap-8">
           <div className="absolute inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_10%,black)]"></div>
           <motion.p className="text-5xl md:text-7xl font-bold tracking-tight relative bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 py-4"
             initial={{ opacity: 0, y: -25 }} whileInView={{ opacity: 1, y: 0 }} viewport={{ once: true }} transition={{ duration: .5, delay: 0, ease: "easeInOut" }}>
             Next.js
           </motion.p>
-          <motion.p className="relative max-w-96 text-lg font-semibold leading-tight tracking-tight text-neutral-800 dark:text-neutral-200 px-4 text-center mt-4"
+          <motion.p className="relative max-w-96 text-lg font-semibold leading-tight tracking-tight text-neutral-800 dark:text-neutral-200 px-4 text-center"
             initial={{ opacity: 0, y: 25 }} whileInView={{ opacity: 1, y: 0 }} viewport={{ once: true }} transition={{ duration: .5, delay: 0, ease: "easeInOut" }}>
             {t.rich('nextjsText', {
               highlight: (chunks) => <span className="text-secondary-400">{chunks}</span>
             })}
           </motion.p>
+          <Miniplayer />
         </div>
-        <Miniplayer />
       </div>
 
       <Divider className="bg-neutral-200 dark:bg-neutral-800" />
 
       {/* tailwind */}
-      <div className="h-auto w-screen flex flex-col items-center bg-white dark:bg-black">
+      <div className="h-auto w-screen flex flex-col items-center bg-white dark:bg-black gap-8">
         <motion.div
-          className="mt-8"
           initial={{ filter: "blur(5px)" }}
           whileInView={{ filter: "blur(0)" }}
           viewport={{ once: true }}
           transition={{ duration: .75, delay: 0, ease: "easeInOut" }}
         >
-          <p className="z-20 text-5xl md:text-9xl font-bold text-center leading-tight tracking-tight from-[#38bdf7] to-[#0531f1] bg-gradient-to-b bg-clip-text text-transparent">
+          <p className="z-20 mt-8 text-5xl md:text-7xl font-bold text-center leading-tight tracking-tight from-[#38bdf7] to-[#0531f1] bg-gradient-to-b bg-clip-text text-transparent">
             Tailwind
           </p>
           <div className="w-[40rem] relative">
@@ -105,7 +102,7 @@ export default function Home() {
           </div>
         </motion.div>
         <motion.p
-          className="max-w-md m-10 mb-16 text-lg font-semibold leading-tight tracking-tight text-center from-sky-600 to-blue-700 bg-gradient-to-b bg-clip-text text-transparent"
+          className="max-w-md mx-8 mb-8 text-lg font-semibold leading-tight tracking-tight text-center from-sky-600 to-blue-700 bg-gradient-to-b bg-clip-text text-transparent"
           initial={{ filter: "blur(5px)", opacity: 0, y: -20 }}
           whileInView={{ filter: "blur(0)", opacity: 1, y: 0 }}
           viewport={{ once: true }}
